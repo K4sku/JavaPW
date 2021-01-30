de541da3c49bf71db81e3d184cc8e81d645cbfd0
Cezary Kłos <cezary.klos@gmail.com>
2021-01-30 22:03:07 +0100

    gotowe zadanie domowe inu3

---
+++ INU/office/src/controller/Main.java
@@ -0,0 +1,44 @@
+package controller;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+	private Stage primaryStage;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		mainWindow();
+	}
+	
+	public void mainWindow() {
+		try {
+			FXMLLoader loader =
+				new FXMLLoader(
+						Main.class.getResource("/view/MainWindowView.fxml")
+						);
+			AnchorPane pane = loader.load();
+			primaryStage.setMinWidth(500.0);
+			primaryStage.setMinHeight(600.0);
+			Scene scene = new Scene(pane);
+			MainWindowController mainWindowController = loader.getController();
+			mainWindowController.setMain(this);
+			mainWindowController.setPrimaryStage(primaryStage);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
